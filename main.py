745186b6028d244e94c1bc3fbd993e358e9269b7
Jere Aho <jjeraho@gmail.com>
2022-07-23 15:17:55 +0300

    refactor to readable

--- main.py
+++ main.py
@@ -15,58 +15,43 @@ CHAR_TEMPL = CHAR_TEMPL[...,::-1]
 PLATFORM_TEMPL = cv2.imread(PLATFORM_TEMPL)
 PLATFORM_TEMPL = PLATFORM_TEMPL[...,::-1]
 
-def startGame():
+
+def start_game():
     play_button = auto.locateOnScreen(PLAY_BUTTON_TEMPL)
     print(play_button)
     if play_button:
         auto.click(play_button)
     print("Game Started!")
 
-def updateLocations():
-    res = cv2.matchTemplate(img2,CHAR_TEMPL,cv2.TM_SQDIFF_NORMED)
-    res2 = cv2.matchTemplate(img2,PLATFORM_TEMPL,cv2.TM_SQDIFF_NORMED)
+
+# Updates object locations
+def update_locations(gameFrame):
+    
+    res = cv2.matchTemplate(gameFrame, CHAR_TEMPL, cv2.TM_SQDIFF_NORMED)
+    res2 = cv2.matchTemplate(gameFrame, PLATFORM_TEMPL, cv2.TM_SQDIFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     loc_platform = np.where(res2 <= 0.15)
     return min_loc, loc_platform
 
-#print("Sleeping...")
-#time.sleep(2)
-#print("Sleep Over!")
-#startGame()
 
-
-w, h = CHAR_TEMPL.shape[0:2]
-w2, h2 = PLATFORM_TEMPL.shape[0:2]
-
-while True:
-    start = time.time_ns()
-    frame = ImageGrab.grab(bbox=GAMEWINDOW)
-    frame_np = np.array(frame)
-    
-
-    img2 = frame_np.copy()
-    
-    top_left, platform_loc = updateLocations()
-
-    for pt in zip(*platform_loc[::-1]):
-        cv2.rectangle(img2, pt, (pt[0] + h2, pt[1] + w2), (0,0,255), 2)
-        
-
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-
-
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv2.rectangle(img2,top_left, bottom_right, 255, 2)
-    
-    cv2.imshow("test",img2)
+# Draw captured game with recognized objects
+def draw_game(gameFrame, charLoc, platformLocs):
+    bottom_right = (charLoc[0] + CHAR_TEMPL.shape[0], charLoc[1] + CHAR_TEMPL.shape[1])
+    cv2.rectangle(gameFrame,charLoc, bottom_right, 255, 2)
+    for pt in zip(*platformLocs[::-1]):
+        cv2.rectangle(gameFrame, pt, (pt[0] + PLATFORM_TEMPL.shape[1], pt[1] + PLATFORM_TEMPL.shape[0]), (0,0,255), 2)
+    cv2.imshow("test", gameFrame)
     cv2.waitKey(1)
     
-    # plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    # plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    # plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    # plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    # plt.suptitle(meth)
-    # plt.show()
-    stop = time.time_ns()
-    print("Time elapsed: {} ms".format((stop-start)/1000000))
+
+def main():
+    while True:
+        start = time.time_ns()
+        gameFrame = np.array(ImageGrab.grab(bbox=GAMEWINDOW))
+        charLoc, platformLocs = update_locations(gameFrame)
+        draw_game(gameFrame, charLoc, platformLocs)
+        stop = time.time_ns()
+        print("Time elapsed for one loop: {} ms".format((stop-start)/1000000))
+
+main()
 
